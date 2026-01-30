fea54263abcd8ec62210eed3fb6cea774171abf1
mt4110 <4221282+mt4110@users.noreply.github.com>
2026-01-30 17:00:04 +0900

    Initial Commit

---
+++ DraftLock/DraftLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraftLockApp.swift
+//  DraftLock
+//
+//  Created by MT on 2026/01/30.
+//
+
+import SwiftUI
+
+@main
+struct DraftLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
